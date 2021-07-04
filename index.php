8fe6ee554beb94870e70328f4172f0943cd7c2a8
Amadou <gayetalib90@gmail.com>
2021-07-04 21:24:23 +0000

    version à jour

--- index.php
+++ index.php
@@ -1,12 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestion d'articles</title>
-</head>
-<body>
-      <h1></h1>
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Gestion des articles</title>
+        
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
+       
+        <link href="assets/css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container px-lg-5">
+                <a class="navbar-brand" href="#!">Gestion d'articles</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>      
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Accueil</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+
+
+       <div class="container">
+           <div class="row">
+               <div class="col-md-3">
+                  <ul style="background-color:#5f6368">
+                      <p style="text-align:center">Les catégories d'article</p>
+                    <li class="nav-item"><a class="nav-link" href="#!"><span style="color:white">Accueil</span> </a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!"><span style="color:white">Informatique</span> </a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!"><span style="color:black">Politique</span> </a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Accueil</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Accueil</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+                  </ul>
+               </div>
+
+               <div class="col-md-9">
+                     <p>Ok Amadou</p>
+               </div>
+                
+           </div>
+       </div>
+
+      
+      
+      
+
+
+
+
+        <footer class="py-3 bg-dark" >
+            <div class="container"><p class="m-0 text-center text-white">Amadou GAYE - DIC2 Info </p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="assets/js/scripts.js"></script>
+    </body>
 </html>
